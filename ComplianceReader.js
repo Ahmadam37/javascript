42fe59fe18cd08f261a5c4e3aea58658bcc25368
Ahmad Binmana <51037193+Ahmadam37@users.noreply.github.com>
2020-01-26 15:19:22 +0300

    Add files via upload

---
+++ ComplianceReader.js
@@ -0,0 +1,19 @@
+﻿function printCounter(counter,current,label,textSuffix,textPrefix)
+{
+	setTimeout(function(){
+	
+							if(current <=counter){
+	 
+												writeHtml(current,label,textSuffix,textPrefix);
+												current = current+1;
+												setTimeout(printCounter(counter,current,label,textSuffix,textPrefix),100);
+												}
+												
+												},100);
+}
+
+function writeHtml(current,label,textSuffix,textPrefix){
+
+	document.getElementById(label).innerHTML = textPrefix + current.toString() + textSuffix ;
+	
+}
